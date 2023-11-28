41c5dfb707193c1615eca3d2b299b273c8db605b
catabimbas <endermenzaogm@gmail.com>
2023-11-28 16:41:05 -0300

    Fim módulo introdutório [Módulos] foi a ultima aula

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const vaitomarnocu = true
-console.log("Hello world!")
-console.log("E eu estou aprendendo")
-if (!vaitomarnocu == false) {
-    console.log("VAI SE FODER")
-}
+var calcFuncao = require("./calculadora")
+
+console.log(calcFuncao.soma(10,20))
+var resultadoMul = calcFuncao.mult(2,2)
+var resultadoSub = calcFuncao.sub(20,2)
+console.log(calcFuncao.div(10,2))
+console.log(resultadoMul)
+console.log(resultadoSub)
+console.log(calcFuncao.nome)
