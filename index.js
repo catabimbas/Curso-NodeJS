e0a742ba21852fd1eb6fc5bfd3974e656b826c17
catabimbas <endermenzaogm@gmail.com>
2023-11-29 15:45:59 -0300

    [Express] Aula 3

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express') //Importanto Express
 const app = express() // Iniciando o express
 
+app.get("/", function (req,res){
+    res.send("VAI SE FUDER")
+})
+
+app.get('/blog',function(req,res){
+    res.send("Bem vindo ao meu blog!")
+})
+app.get('/canal/youtube', function(req, res){
+    res.send("<h1>Bem vindo ao meu canal</h1>")
+})
+
+
+
 app.listen(4000,function (erro){
     erro?console.log("Ocorreu um erro"):console.log("Servidor iniciado com sucesso")
 })
