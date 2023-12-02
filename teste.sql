6f6008ebb5d41b8675a2c0ec037b1d42b3d9765a
catabimbas <endermenzaogm@gmail.com>
2023-12-02 15:39:51 -0300

    [MySql] UPDATE - Aula 05 final

--- teste.sql
+++ teste.sql
@@ -1,5 +1,5 @@
 /* COMANDOS */
-/* Excluir dados ou tabelas */
-DELETE FROM usuarios; /* A tabela vai ser excluída */
-DELETE FROM usuarios WHERE idade; /* A coluna será excluída */
-DELETE FROM usuarios WHERE nome = "Victor Lima"; /* Excluirá uma linha específica */
+/* Atualizar uma coluna inteira */
+UPDATE usuarios SET nome = "Nome de Teste"
+/* Atualizar um dado específico */
+UPDATE usuarios SET nome = "Nome de Teste" WHERE nome = "Victor Lima"
