6dedcaf048db87bb857ac807377e20da0d483554
catabimbas <endermenzaogm@gmail.com>
2023-12-02 15:22:47 -0300

    [MySql] DELETE - Aula 04

--- teste.sql
+++ teste.sql
@@ -1,11 +1,5 @@
 /* COMANDOS */
-/* Inserir dados na table */
-INSERT INTO usuarios(idade, email, nome) Values(
-    19,
-    "email@teste.com",
-    "Victor Silva"
-);
-/* Listando os dados da table */
-SELECT * FROM usuarios; /* Selecione todos os dados de table */
-/* Especificando dado */
-SELECT * FROM usuarios WHERE idade = 8;
+/* Excluir dados ou tabelas */
+DELETE FROM usuarios; /* A tabela vai ser excluída */
+DELETE FROM usuarios WHERE idade; /* A coluna será excluída */
+DELETE FROM usuarios WHERE nome = "Victor Lima"; /* Excluirá uma linha específica */
