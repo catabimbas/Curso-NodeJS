236dc233856543b1b8e7a6554e95bfa0343f82f6
catabimbas <endermenzagm@gmail.com>
2023-12-12 16:06:50 -0300

    [Módulo 5 mão na massa] EJS e criação de projetos - Aula 2

--- teste.sql
+++
@@ -1,5 +0,0 @@
-/* COMANDOS */
-/* Atualizar uma coluna inteira */
-UPDATE usuarios SET nome = "Nome de Teste"
-/* Atualizar um dado específico */
-UPDATE usuarios SET nome = "Nome de Teste" WHERE nome = "Victor Lima"
