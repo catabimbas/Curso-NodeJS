94c4555a18f22eaa3111e799b804a9a62911cb66
catabimbas <endermenzaogamer@gmail.com>
2023-12-30 20:09:09 -0300

    Como instalar - Modulo 5 Aula 8

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+ const express = require("express")
 const app = express()
 
 // Estou dizendo para o Express usar o EJS como view engine
@@ -6,36 +6,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get("/", (req,res)=>{
-    var nome = "Victor lima"
-    var lang = "JS"
-    var exibirMsg = false
-
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-cola", preco: 5},
-        {nome: "Leite", preco: 1.45},
-        {nome: "Carne", preco: 15},
-        {nome: "Redbull", preco: 6}
-    ]
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do programador",
-        msg: exibirMsg,
-        produtos:produtos
-    })
-})
-app.get("/:nome/:lang", (req,res)=>{
-    var nome = req.params.nome
-    var lang = req.params.lang
-    res.render("home",{
-        nome: nome,
-        lang: lang
-    })
-})
-app.get("/usuario", (req,res)=>{
-    res.render("principal/perfil")
+    res.render("index")
 })
 
 app.listen (8080, ()=>{console.log("App rodando!")})
