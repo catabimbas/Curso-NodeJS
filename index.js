04b4388c10f5c357a946e3dc78dd340dcd180f95
catabimbas <endermenzagm@gmail.com>
2024-01-10 09:43:26 -0300

    Como capturar os dados [Projeto] - Modulo 5 Aula 15

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
- const express = require("express")
+const express = require("express")
+const bosyParser = require("body-parser")
+const bodyParser = require("body-parser")
 const app = express()
 
 // Estou dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+// Estou dizendo para o Express usar o BodyParser como decodificador de dados do formulário
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 app.get("/", (req,res)=>{
     res.render("index")
@@ -14,6 +20,10 @@ app.get("/perguntar", (req,res)=>{
 })
 
 app.post("/salvarPergunta", (req,res)=>{
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    console.log(titulo)
+    console.log(descricao)
     res.send("Formulario recebido")
 })
 
