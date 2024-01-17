e9ddc0ce882668ff9ddee4e7f8258785b64d0082
catabimbas <endermenzaogamer@gmail.com>
2024-01-17 16:38:15 -0300

    Model Resposta [Projeto] - Modulo 5 Aula 25

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require("body-parser")
 const connection = require('./database/database')
 const Pergunta = require("./database/Perguntar")
+const Resposta = require("./database/Resposta")
 
 // Database
 connection
