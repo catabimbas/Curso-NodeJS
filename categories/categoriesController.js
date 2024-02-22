8153851de3dedadcb34e097f9871473db4ca93da
catabimbas <endermenzaogamer@gmail.com>
2024-02-22 17:54:35 -0300

    Form de Edição [Projeto 2] - Modulo 6 Aula 16

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save",(req,res)=>{
             title:title,
             slug: slugfy(title) //Putaria Gráis -> putaria-gratis
         }).then(()=>{
-            res.redirect("/")
+            res.redirect("/admin/categories")
         })
     }else{
         res.redirect("/admin/categories/new")
@@ -46,6 +46,20 @@ router.post('/categories/delete',(req,res)=>{
         res.redirect("/admin/categories")
     }
     
+})
+
+router.get("/admin/categories/edit/:id", (req,res)=>{
+    var id = req.params.id
+    if(isNaN(id)){
+        res.redirect("/admin/categories")
+    }
+    Category.findByPk(id).then(category=>{
+        if(category != undefined){
+            res.render("admin/categories/edit",{category: category})
+        }else{
+            res.redirect("/admin/categories")
+        }
+    })
 })
 
 module.exports = router
